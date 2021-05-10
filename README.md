8c48784fd6345640464f500a59972977e4287a91
DESKTOP-VFTGTTV\Winkers <smmehdisharifi@gmail.com>
2021-05-10 13:02:45 +0430

    [Add]: add progressbar and shuffle output and edit README.md

--- README.md
+++ README.md
@@ -13,24 +13,27 @@ pip3 install -r requirements.txt
 ## Usage
 
 ```
-usage: backup_link.py [-h] [-e EXTENSIONS [EXTENSIONS ...]] -f INPUT_FILEPATH [-o OUTPUT_FILEPATH] [-q]
+usage: backup_link.py [-h] [-e EXTENSIONS [EXTENSIONS ...]] [-s] -f INPUT_FILEPATH
+                      [-o OUTPUT_FILEPATH] [-q]
 
 Backup Link
 
 optional arguments:
   -h, --help            show this help message and exit
   -e EXTENSIONS [EXTENSIONS ...], --extensions EXTENSIONS [EXTENSIONS ...]
-                        extensions that should supported in output. Default:['zip', 'tar', 'tar.gz', 'rar', 'sql', 'gzip', '7z', 'gz', 'bz2']
+                        extensions that should supported in output. default=["zip",
+                        "tar", "tar.gz", "rar", "sql", "gzip", "7z", "gz", "bz2"]
+  -s, --shuffle         shuffle output results. default=False
   -f INPUT_FILEPATH, --file INPUT_FILEPATH
                         file containing urls, 1 host per line.
   -o OUTPUT_FILEPATH, --output OUTPUT_FILEPATH
-                        file to save results.
+                        file to save results. default="output.txt"
   -q, --quiet           not print status messages to stdout. Default=True
 ```
 
 ## Example
 ```bash
-python3 backup_link.py -e zip rar -f input.txt -o output.txt
+python3 backup_link.py -e zip rar -f input.txt -o output.txt -s
 ```
 
 ## Contributing
--- backup_link.py
+++ backup_link.py
@@ -5,29 +5,40 @@ import re
 import sys
 import time
 
+from progressbar import progressbar
+
 
 class BackupLink:
     """MikrotikExtractor class object"""
 
-    def __init__(self, extensions, input_filepath, output_filepath, verbose):
+    def __init__(self, extensions, shuffle, input_filepath, output_filepath, verbose):
         """Initialize MikrotikExtractor class object."""
 
         self.extensions = extensions
+        self.shuffle = shuffle
         with open(input_filepath) as self.input_file:
             self.urls = self.input_file.read().splitlines()
         self.output_filepath = output_filepath
         self.verbose = verbose
-        self.backup_links = []
+        self.backups = []
 
-    def create_output(self):
+    def append_output(self, backups):
         """Create output for links"""
-        with open(self.output_filepath, 'w') as file:
-            file.writelines('\n'.join(self.backup_links))
+        with open(self.output_filepath, 'a') as file:
+            file.writelines('\n'.join(backups) + '\n')
+
+    def create_shuffle_output(self):
+        random.shuffle(self.backups)
+        with open('shuffle-' + self.output_filepath, 'a') as file:
+            file.writelines('\n'.join(self.backups))
 
     def run(self):
         """Run backup link"""
 
-        for index, url in enumerate(self.urls):
+        if not self.verbose:
+            self.urls = progressbar(self.urls)
+
+        for i, url in enumerate(self.urls):
             m = re.search(
                 '(?P<protocol>https?://)?(?P<domain>[A-Za-z_0-9-]+\.)(?P<top_level_domain>([A-Za-z]+\.)?[A-Za-z]+).*',
                 url)
@@ -36,6 +47,9 @@ class BackupLink:
                 domain = m.group('domain')
                 top_level_domain = m.group('top_level_domain')
 
+                if self.verbose:
+                    print(f'[+] Processing {url} ({i + 1}/{len(self.urls)})')
+
                 for extension in self.extensions:
                     backups = [
                         f'{protocol}{domain}{top_level_domain}/{domain}{extension}',
@@ -43,62 +57,68 @@ class BackupLink:
                         f'{protocol}{domain}{top_level_domain}/www.{domain}{extension}',
                         f'{protocol}{domain}{top_level_domain}/www.{domain}{top_level_domain}.{extension}',
                     ]
-                    for backup in backups:
-                        self.backup_links.append(backup)
 
-                random.shuffle(self.backup_links)
+                    self.append_output(backups)
+                    self.backups = self.backups + backups
 
-                for backup in self.backup_links:
-                    if self.verbose:
-                        print(backup)
-
-        if self.output_filepath:
-            self.create_output()
+        if self.shuffle:
+            self.create_shuffle_output()
 
 
 def get_timestamp():
     """Retrieve a pre-formatted datetime."""
 
     now = time.localtime()
-    timestamp = time.strftime("%Y%m%d_%H%M%S", now)
+    timestamp = time.strftime('%Y%m%d_%H%M%S', now)
     return timestamp
 
 
 def run():
     """Run entrypoint."""
 
-    parser = argparse.ArgumentParser(description="Backup Link")
+    parser = argparse.ArgumentParser(description='Backup Link')
     parser.add_argument(
-        "-e", "--extensions", dest="extensions", action="store", nargs="+",
-        default=["zip", "tar", "tar.gz", "rar", "sql", "gzip", "7z", "gz", "bz2"],
-        help="extensions that should supported in output."
-             " Default:['zip', 'tar', 'tar.gz', 'rar', 'sql', 'gzip', '7z', 'gz', 'bz2']"
+        '-e', '--extensions', dest='extensions', action='store', nargs='+',
+        default=['zip', 'tar', 'tar.gz', 'rar', 'sql', 'gzip', '7z', 'gz', 'bz2'],
+        help='extensions that should supported in output. '
+             'default=["zip", "tar", "tar.gz", "rar", "sql", "gzip", "7z", "gz", "bz2"]'
     )
     parser.add_argument(
-        "-f", "--file", dest="input_filepath", action="store", required=True,
-        help="file containing urls, 1 host per line."
+        '-s', '--shuffle', dest='shuffle', action="store_true", required=False, default=False,
+        help='shuffle output results. default=False',
     )
     parser.add_argument(
-        "-o", "--output", dest="output_filepath", action="store", help="file to save results."
+        '-f', '--file', dest='input_filepath', action='store', required=True,
+        help='file containing urls, 1 host per line.'
     )
-    parser.add_argument("-q", "--quiet", action="store_false", dest="verbose", default=True,
-                        help="not print status messages to stdout. Default=True")
+    parser.add_argument(
+        '-o', '--output', dest='output_filepath', action='store', default='output.txt',
+        help='file to save results. default="output.txt"'
+    )
+    parser.add_argument('-q', '--quiet', action='store_false', dest='verbose', default=True,
+                        help='not print status messages to stdout. Default=True')
 
     args = parser.parse_args()
 
+    if args.verbose:
+        print(f'[+] Initiation timestamp: {get_timestamp()}')
+
     if not os.path.exists(args.input_filepath):
-        print("[-] Specify a valid filepath containing urls with -f")
+        print('[-] Specify a valid filepath containing urls with -f')
         sys.exit(0)
 
-    if args.verbose:
-        print(f"[*] Initiation timestamp: {get_timestamp()}")
+    if os.path.exists(args.output_filepath):
+        print(f'[*] File [{args.output_filepath}] has already exists. urls will write in append mode.')
+
+    if args.shuffle and os.path.exists('shuffle-' + args.output_filepath):
+        print(f'[*] File [shuffle-{args.output_filepath}] has already exists. urls will write in append mode.')
 
     mikrotik_extractor = BackupLink(**vars(args))
     mikrotik_extractor.run()
 
     if args.verbose:
-        print(f"[*] Completion timestamp: {get_timestamp()}")
-        print('[*] Done')
+        print(f'[+] Completion timestamp: {get_timestamp()}')
+        print('[+] Done')
 
 
 if __name__ == '__main__':
